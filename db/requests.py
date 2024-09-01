4f07d7d9cc48830d5c55684b46113ea04a8b187d
Ivan <gordeevid@yandex.ru>
2024-09-01 12:40:51 +0300

    fix problems

--- db/requests.py
+++ db/requests.py
@@ -2,15 +2,15 @@ from sqlalchemy import select, update, delete
 from db.models import Users, Words, Messages, UsersWords, async_session
 
 async def fill_default_values(data:dict)->None:
-    for words in data['Words']:
-        await add_word(words[1], words[0])
+    for id_word, words in enumerate(data['Words']):
+        await add_word(id_word+1, words[1], words[0])
     for message_id, message in data['Messages'].items():
         await add_message(message_id, message)
         # print('You need to restart program!')
 
-async def add_word(word_ru:str, word_en:str):
+async def add_word(id, word_ru:str, word_en:str):
     async with async_session() as session:
-        obj = Words(word_ru = word_ru, word_en = word_en)
+        obj = Words(id = id,word_ru = word_ru, word_en = word_en)
         session.add(obj)
         try:
             await session.commit()
@@ -68,8 +68,8 @@ async def add_users_word(word_ru:str, word_en:str, user_id:int)->bool:
                 await session.commit()
             except:
                 return False
-            new_word = await session.scalar(select(Words).where(Words.word_ru == word_ru, Words.word_en == word_en))
-            new_word_id = new_word.id
+        new_word = await session.scalar(select(Words).where(Words.word_ru == word_ru, Words.word_en == word_en))
+        new_word_id = new_word.id
         userword = await session.scalar(select(UsersWords).where(UsersWords.word_id == new_word_id))
         if not userword:
             session.add(UsersWords(user_id = user_id, word_id = new_word_id))
@@ -90,8 +90,9 @@ async def delete_word_from_db(word_ru:str, word_en:str, user_id:int)->bool:
                 users_words = await session.scalars(select(UsersWords).where(UsersWords.word_id == found_word_id))
                 if users_words:
                     only_user = True
-                    if len(users_words.all()) > 1:
-                        for word in users_words.all():
+                    users_words_data = users_words.all()
+                    if len(users_words_data) > 1:
+                        for word in users_words_data:
                             if word.user_id != user_id:
                                 only_user = False
                     if only_user:
@@ -119,10 +120,10 @@ async def delete_word_from_db(word_ru:str, word_en:str, user_id:int)->bool:
 async def get_userwords(user_id:str)->list | None:
     async with async_session() as session:
             result_list = []
-            userwords = await session.scalars(select(UsersWords).where(UsersWords.user_id == user_id))
-            for word in userwords.all():
-                q_words = await session.scalar(select(Words).where(Words.id == word.word_id))
-                result_list.append((q_words.word_ru, q_words.word_en))
+            userwords = await session.scalars(select(Words).join(UsersWords).where(Words.id == UsersWords.word_id, UsersWords.user_id == user_id))
+            result = userwords.all()
+            for word in result:
+                result_list.append((word.word_ru, word.word_en))
             return result_list
 
 
